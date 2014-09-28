a7ebde6c61d5d856fd5dd326f5cc578adfcb5969
Anand Rajneesh <anandrajneesh@outlook.com>
2014-09-28 17:26:04 +0200

    tests

---
+++ test/notobject.js
@@ -0,0 +1,10 @@
+/**
+ * Created by ARA8376 on 28/09/2014.
+ */
+
+
+var result = {'somethin':'somethinf'};
+var un = undefined;
+
+console.log(!!result);
+console.log(!!un)
---
+++ test/sameargsincallbacks.js
@@ -0,0 +1,12 @@
+/**
+ * Created by ARA8376 on 26/09/2014.
+ */
+function one(result,cb){
+    cb(result);
+}
+
+
+
+function two(){
+
+}
